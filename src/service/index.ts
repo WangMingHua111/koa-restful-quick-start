9707f27f33763953725ecdd5504d3ccb1179807b
王明华 <735161977@qq.com>
2023-12-14 13:31:51 +0800

    modified:   src/core/swagger.ts 	modified:   src/service/index.ts

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,10 +1,12 @@
-import { globSync } from 'glob'
-import * as path from 'path'
+// import { globSync } from 'glob'
+// import * as path from 'path'
 
-const files = globSync(['**/*-controller.ts'], { cwd: __dirname })
+// const files = globSync(['**/*-controller.ts'], { cwd: __dirname })
 
-// 自动导入所有控制器
-files.forEach(async (f) => {
-    // console.log(`import ${path.resolve(__dirname, f)}`)
-    await import(path.resolve(__dirname, f))
-})
+// // 自动导入所有控制器
+// files.forEach(async (f) => {
+//     // console.log(`import ${path.resolve(__dirname, f)}`)
+//     await import(path.resolve(__dirname, f))
+// })
+
+import './demo-controller'
